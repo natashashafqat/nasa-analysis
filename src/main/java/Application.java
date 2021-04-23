9e5449c11992edd893b02fa4a125736994bd4e64
natashashafqat <natashashafqat@gmail.com>
2021-04-23 09:37:00 +0100

    feat: opening EPIC images in default browser;

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,18 +1,23 @@
 import epic.ImageUrlBuilder;
 
+import java.awt.*;
 import java.io.IOException;
+import java.net.URI;
 import java.util.List;
 
 public class Application {
     public static void main(String args[]) throws IOException {
-        getEpicImages();
+        ImageUrlBuilder imageUrlBuilder = new ImageUrlBuilder();
+        List<String> epicImages = imageUrlBuilder.getImageUrls();
+        openEpicImages(epicImages);
     }
 
-    private static void getEpicImages() throws IOException {
-        ImageUrlBuilder imageUrlBuilder = new ImageUrlBuilder();
-        List<String> imageUrls = imageUrlBuilder.getImageUrls();
-        for (String url : imageUrls) {
-            System.out.println(url);
+    private static void openEpicImages(List<String> imageUrls) throws IOException {
+        if (Desktop.isDesktopSupported() && Desktop.getDesktop().isSupported(Desktop.Action.BROWSE)) {
+            for (String url : imageUrls) {
+                System.out.println("Opening: " + url);
+                Desktop.getDesktop().browse(URI.create(url));
+            }
         }
     }
 }
